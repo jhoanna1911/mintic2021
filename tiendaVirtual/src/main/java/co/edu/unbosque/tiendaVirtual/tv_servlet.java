e1fd0d04a5148fca7b7260b99f72964fdc4ddf40
Lorena Alfonso <jlalfonso@itc.com.co>
2021-09-09 00:39:26 -0500

    proyecto en eclipse con servlet y jsp

---
+++ tiendaVirtual/src/main/java/co/edu/unbosque/tiendaVirtual/tv_servlet.java
@@ -0,0 +1,51 @@
+package co.edu.unbosque.tiendaVirtual;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class tv_servlet
+ */
+@WebServlet("/tv_servlet")
+public class tv_servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public tv_servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String Usuario = request.getParameter("Usuario");
+		String Contrasena = request.getParameter("Contrasena");
+		PrintWriter writer = response.getWriter();
+		
+		if (Usuario != null && Contrasena != null) {
+			writer.println("Bienvenido " + Usuario + " a la tienda virtual");
+		}
+		else 
+			writer.println("Debe llenar todos los campos");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ tiendaVirtual/src/main/webapp/index.jsp
@@ -0,0 +1,56 @@
+<!doctype html>
+
+<html lang="es">
+
+<head>
+
+    <meta charset="utf-8">
+
+    <title> Formulario de Acceso </title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Overpass&display=swap" rel="stylesheet">
+
+    <!-- Link hacia el archivo de estilos css -->
+    <link rel="stylesheet" href="css/login.css">
+
+    <style type="text/css">
+
+    </style>
+
+    <script type="text/javascript">
+    </script>
+
+</head>
+
+<body>
+
+    <div id="contenedor">
+        <div id="central">
+            <div id="login">
+                <div class="titulo">
+                    Bienvenido
+                </div>
+                <form id="loginform" method="get" action ="./tv_servlet">
+                    <input type="text" name="Usuario" placeholder="Usuario" required>
+
+                    <input type="password" placeholder="Contrasena" name="Contrasena" required>
+
+                    <button type="submit" title="Ingresar" name="Ingresar">Ingresar</button>
+                </form>
+                <div class="pie-form">
+                    <a href="#">¿Perdiste tu contraseña?</a>
+                    <a href="#">¿No tienes Cuenta? Registrate</a>
+                </div>
+            </div>
+            <div class="inferior">
+                <a href="#">Volver</a>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
